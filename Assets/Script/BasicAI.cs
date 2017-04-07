9015760560107570d12112a2dd5f4493b743c19b
Arif Budiman <arif.budiman41@ui.ac.id>
2017-04-07 17:40:39 +0700

    add patrol function

--- Assets/Script/BasicAI.cs
+++ Assets/Script/BasicAI.cs
@@ -9,54 +9,129 @@ public class BasicAI : MonoBehaviour {
     
 
     public GameObject player;
-    public Sprite[] sprt;
+    public Sprite[] spirte = new Sprite[4];
     public float delay = 0.1f;
+    public float speed = 5f;
     public LayerMask obstacles;
 
     float del;
     float angle;
     float distance;
     bool lookedPlayer;
+    bool onChangeDirection;
 
-    public Direction eDir;
-    public Vector2 vDir = Vector2.zero;
+    public Direction direction;
+    Vector2 vDir = Vector2.zero;
 
     // Use this for initialization
     void Start () {
         del = delay;
         myRndr = this.GetComponent<SpriteRenderer>();
-	}
+        body = this.GetComponent<Rigidbody2D>();
+        onChangeDirection = false;
 
-    // Update is called once per frame
-    void FixedUpdate() {
-        del -= Time.fixedDeltaTime;
-       
-        if (del < 0)
+
+        if(body != null)
         {
-            del = delay;
-            //angle = Vector2.Distance(this.transform.position, player.transform.position);
-
-            switch (eDir)
-            {
-                case Direction.up:
-                    vDir = Vector2.up;
-                    break;
-                case Direction.left:
-                    vDir = Vector2.left;
-                    break;
-                case Direction.right:
-                    vDir = Vector2.right;
-                    break;
-                case Direction.down:
-                    vDir = Vector2.down;
-                    break;
-            }
-            VisualChecked();
-
-            Debug.Log(distance);
-            //Debug.Log(angle);
+            body.freezeRotation = true;
         }
 	}
+
+    void Update()
+    {
+        PatrolAI();
+    }
+
+    void FixedUpdate() {
+        del -= Time.fixedDeltaTime;
+
+        UpdateVectorDirection();
+
+        // update spirte
+        switch (direction)
+        {
+            case Direction.up:
+                myRndr.sprite = spirte[(int)Direction.up];
+                break;
+            case Direction.left:
+                myRndr.sprite = spirte[(int)Direction.left];
+                break;
+            case Direction.right:
+                myRndr.sprite = spirte[(int)Direction.right];
+                break;
+            case Direction.down:
+                myRndr.sprite = spirte[(int)Direction.down];
+                break;
+        }
+        // cause only have left sprite
+        if (direction == Direction.right)
+        {
+            myRndr.flipX = true;
+        }
+        else
+        {
+            myRndr.flipX = false;
+        }
+
+        VisualChecked();
+
+        //Debug.Log(distance);
+        //Debug.Log(angle);
+    }
+
+    public void UpdateVectorDirection()
+    {
+        switch (direction)
+        {
+            case Direction.up:
+                vDir = Vector2.up;
+                break;
+            case Direction.left:
+                vDir = Vector2.left;
+                break;
+            case Direction.right:
+                vDir = Vector2.right;
+                break;
+            case Direction.down:
+                vDir = Vector2.down;
+                break;
+        }
+    }
+
+    public void PatrolAI()
+    {
+        body.velocity = vDir * speed;
+        if(!onChangeDirection)
+            StartCoroutine(ChangeRandomDirection(Random.Range(2f, 5f)));
+    }
+
+    IEnumerator ChangeRandomDirection(float time)
+    {
+        onChangeDirection = true;
+        yield return new WaitForSeconds(time);
+        RandomDirection();
+        onChangeDirection = false;
+    }
+
+    public void RandomDirection()
+    {
+        int rnd = Random.Range(0, 4);
+        switch (rnd)
+        {
+            case (int)Direction.up:
+                direction = Direction.up;
+                break;
+            case (int)Direction.left:
+                direction = Direction.left;
+                break;
+            case (int)Direction.right:
+                direction = Direction.right;
+                break;
+            case (int)Direction.down:
+                direction = Direction.down;
+                break;
+        }
+    }
 
     public void VisualChecked()
     {
@@ -68,7 +143,10 @@ public class BasicAI : MonoBehaviour {
         RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + vDir * 1.0f, vDir * 8);
         if (hit.collider != null)
         {
-            lookedPlayer = distance < 6.0f && hit.collider.gameObject.CompareTag("Player");
+            lookedPlayer = angle < 60 && distance < 6.0f && hit.collider.gameObject.CompareTag("Player");
+        }else
+        {
+            lookedPlayer = angle < 60 && distance < 6.0f;
         }
     }
 }
